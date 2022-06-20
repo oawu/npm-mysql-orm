ccfb9cbdfdfe986d08b4677ab867b5309eceaad4
OA Wu <oa.wu@idrip.coffee>
2022-06-20 17:52:31 +0800

    Fix author Fix copyright

--- migrations/002-insert User.js
+++ migrations/002-insert User.js
@@ -1,6 +1,6 @@
 /**
- * @author      OA Wu <comdan66@gmail.com>
- * @copyright   Copyright (c) 2015 - 2020, Ginkgo
+ * @author      OA Wu <oawu.tw@gmail.com>
+ * @copyright   Copyright (c) 2015 - 2022, Ginkgo
  * @license     http://opensource.org/licenses/MIT  MIT License
  * @link        https://www.ioa.tw/
  */
